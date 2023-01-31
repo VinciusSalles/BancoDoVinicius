721b1b87774785532e99fa810e5d40a8f7862bf1
VinciusSalles <vinciusalles@hotmail.com>
2023-01-31 16:15:52 -0300

    projetobanco

---
+++ Banco.java
@@ -0,0 +1,14 @@
+package banco;
+
+import modelos.Conta;
+
+public class Banco {
+
+	public static void main(String[] args) {
+		
+		Conta A001 = new Conta("ContaVinicius");
+		System.out.println(A001.toString());
+
+	}
+
+}
